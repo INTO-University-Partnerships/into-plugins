99012c015c04126bcfa383988d7980c69a28ff92
Mike McGowan <mike.mcgowan@intoglobal.com>
2016-02-03 17:24:45 +0000

    Prevented the presence of the Django test database from breaking a grep

--- vagrant/provision_databases.sh
+++ vagrant/provision_databases.sh
@@ -3,11 +3,11 @@
 MYSQL_DB_PASSWORD="W0mb4t666!"
 
 # Create (empty) MySQL databases
-SHOW=`echo "show databases" | mysql -uroot -p"$MYSQL_DB_PASSWORD" | grep plugins_django`
+SHOW=`echo "show databases" | mysql -uroot -p"$MYSQL_DB_PASSWORD" | grep -P '^plugins_django$'`
 if [ "$SHOW" != "plugins_django" ]; then
     echo "create database plugins_django" | mysql -uroot -p"$MYSQL_DB_PASSWORD"
 fi
-SHOW=`echo "show databases" | mysql -uroot -p"$MYSQL_DB_PASSWORD" | grep plugins_moodle`
+SHOW=`echo "show databases" | mysql -uroot -p"$MYSQL_DB_PASSWORD" | grep -P '^plugins_moodle$'`
 if [ "$SHOW" != "plugins_moodle" ]; then
     echo "create database plugins_moodle" | mysql -uroot -p"$MYSQL_DB_PASSWORD"
 fi
