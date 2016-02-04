44974a98f55db0e7b1b9ae9ad6bf454c02489004
Mike McGowan <mike.mcgowan@intoglobal.com>
2016-02-04 15:07:03 +0000

    Added output to provision_databases.sh

--- vagrant/provision_databases.sh
+++ vagrant/provision_databases.sh
@@ -2,16 +2,30 @@
 
 MYSQL_DB_PASSWORD="W0mb4t666!"
 
-# Restore MySQL databases
+# Restore Django MySQL database
 SHOW=`echo "show databases" | mysql -uroot -p"$MYSQL_DB_PASSWORD" | grep -P '^plugins_django$'`
 if [ "$SHOW" != "plugins_django" ]; then
+    echo "Django database does not exist, creating ..."
     echo "create database plugins_django" | mysql -uroot -p"$MYSQL_DB_PASSWORD"
+    echo "Done."
+    echo "Restoring Django database ..."
     mysql -uroot -p"$MYSQL_DB_PASSWORD" plugins_django < /home/vagrant/django.sql
+    echo "Done."
+else
+    echo "Django database already exists."
 fi
 rm /home/vagrant/django.sql
+
+# Restore Moodle MySQL database
 SHOW=`echo "show databases" | mysql -uroot -p"$MYSQL_DB_PASSWORD" | grep -P '^plugins_moodle$'`
 if [ "$SHOW" != "plugins_moodle" ]; then
+    echo "Moodle database does not exist, creating ..."
     echo "create database plugins_moodle" | mysql -uroot -p"$MYSQL_DB_PASSWORD"
+    echo "Done."
+    echo "Restoring Moodle database ..."
     mysql -uroot -p"$MYSQL_DB_PASSWORD" plugins_moodle < /home/vagrant/moodle.sql
+    echo "Done."
+else
+    echo "Moodle database already exists."
 fi
 rm /home/vagrant/moodle.sql
