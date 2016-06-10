18e9dd1d8445ee2e8c1d6fc3eef15d82a381cf2f
Mike McGowan <mike.mcgowan@intoglobal.com>
2016-06-10 17:09:30 +0100

    Prevented a MySQL 5.6 warning

--- vagrant/provision_databases.sh
+++ vagrant/provision_databases.sh
@@ -30,29 +30,29 @@ else
 fi
 
 # MySQL
-MYSQL_DB_PASSWORD="W0mb4t666!"
+export MYSQL_PWD="W0mb4t666!"
 
 # Restore Django MySQL database
-SHOW=`echo "show databases" | mysql -uroot -p"$MYSQL_DB_PASSWORD" | grep -P '^plugins_django$'`
+SHOW=`echo "show databases" | mysql -uroot | grep -P '^plugins_django$'`
 if [ "$SHOW" != "plugins_django" ]; then
     echo "MySQL Django database does not exist, creating ..."
-    echo "create database plugins_django" | mysql -uroot -p"$MYSQL_DB_PASSWORD"
+    echo "create database plugins_django" | mysql -uroot
     echo "Done."
     echo "Restoring Django database ..."
-    mysql -uroot -p"$MYSQL_DB_PASSWORD" plugins_django < /vagrant/vagrant/django.mysql
+    mysql -uroot plugins_django < /vagrant/vagrant/django.mysql
     echo "Done."
 else
     echo "MySQL Django database already exists."
 fi
 
 # Restore Moodle MySQL database
-SHOW=`echo "show databases" | mysql -uroot -p"$MYSQL_DB_PASSWORD" | grep -P '^plugins_moodle$'`
+SHOW=`echo "show databases" | mysql -uroot | grep -P '^plugins_moodle$'`
 if [ "$SHOW" != "plugins_moodle" ]; then
     echo "MySQL Moodle database does not exist, creating ..."
-    echo "create database plugins_moodle" | mysql -uroot -p"$MYSQL_DB_PASSWORD"
+    echo "create database plugins_moodle" | mysql -uroot
     echo "Done."
     echo "Restoring Moodle database ..."
-    mysql -uroot -p"$MYSQL_DB_PASSWORD" plugins_moodle < /vagrant/vagrant/moodle.mysql
+    mysql -uroot plugins_moodle < /vagrant/vagrant/moodle.mysql
     echo "Done."
 else
     echo "MySQL Moodle database already exists."
@@ -62,3 +62,5 @@ fi
 /home/vagrant/.virtualenvs/plugins_py2/bin/python2.7 /vagrant/manage.py migrate
 cd /vagrant/moodle
 php admin/cli/upgrade.php --non-interactive
+
+unset MYSQL_PWD
